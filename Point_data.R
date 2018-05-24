b6d32694f24d3c08073ddf9ff857cfd48dee425c
Stewart Wilson <stuwilson@ucdavis.edu>
2018-05-24 08:03:41 -0700

    this is the git for the point data for the sierra project

---
+++ Point_data.R
@@ -0,0 +1,16 @@
+setwd("C:/Users/stuwi/Dropbox/Sierra Project/")
+library(rgdal)
+library(raster)
+library(maptools)
+library(reshape)
+library(rgeos)
+library(cdlTools)
+
+# there is a lot of garbage in the way ryans files were set up
+
+data_1 = read.csv("Ferrell_R Master Data Set_for stu1_CSV.csv", skip = 1, header = T, nrows = 1, as.is = T)
+
+
+df = read.csv(file, skip = 3, header = F)
+colnames(df)= headers
+read
